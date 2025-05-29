ce5190a4dc24ad7db75440c3af7effa2d1a045dc
gabriel murillo <gamurigm@gmail.com>
2025-05-29 14:40:13 -0500

    Documentacion completa sin actualizar

--- SistemaForestalFinal/src/main/java/com/mycompany/sistemaforestalfinal/controller/LoginServlet.java
+++ SistemaForestalFinal/src/main/java/com/mycompany/sistemaforestalfinal/controller/LoginServlet.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package com.mycompany.sistemaforestalfinal.controller;
 
 import com.mycompany.sistemaforestalfinal.model.Usuario;
